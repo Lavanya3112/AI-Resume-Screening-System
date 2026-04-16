de42fab0febe8a52757e5b4b3ece42f9030f5ecd
Lavanya Dive <lavanyadive3112@gmail.com>
2026-04-16 22:52:32 +0530

    Create data.py

---
+++ data/data.py
@@ -0,0 +1,20 @@
+RESUMES = {
+    "Strong Candidate": """
+    Data Scientist with 5 years experience.
+    Skills: Python, Machine Learning, Deep Learning, NLP, SQL.
+    Tools: TensorFlow, PyTorch, Pandas.
+    """,
+
+    "Average Candidate": """
+    Data Analyst with 2 years experience.
+    Skills: Python, Excel, SQL.
+    Tools: Pandas, Power BI.
+    """,
+
+    "Weak Candidate": """
+    Fresher with basic knowledge.
+    Skills: Excel.
+    """
+}
+
+JOB_SKILLS = ["python", "machine learning", "nlp", "sql"]
